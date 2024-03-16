e92594673becbc2d5b52e7042c5a703ce059468d
kaikerochadev <kaikerochadev@gmail.com>
2024-03-15 21:16:05 -0300

    Validate Store User

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,11 +6,21 @@
 
 <h1>Novo Usuário</h1>
 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form action="{{ route('users.store') }}" method="POST">
     @csrf
-    <input type="name" name="name" placeholder="Nome:">
-    <input type="email" name="email" placeholder="E-mail:">
-    <input type="password" name="password" placeholder="Senha:">
+    <input type="name" name="name" placeholder="Nome:" value="{{old('name')}}">
+    <input type="email" name="email" placeholder="E-mail:" value="{{old('email')}}">
+    <input type="password" name="password" placeholder="Senha:" value="{{old('password')}}">
     <button>
         Cadastrar
     </button>
