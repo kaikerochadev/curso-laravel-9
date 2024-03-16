cf2256938e6a3edbdb1a8645fe268278d761760c
kaikerochadev <kaikerochadev@gmail.com>
2024-03-16 19:56:29 -0300

    Correcting route name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,8 +16,6 @@ class UserController extends Controller
 
     public function show($id) {
         // $user = User::where('id', $id)->first();
-        
-        $user = User::find($id);
 
         if (!$user = User::find($id)) {
             return redirect()->route('users.index');
@@ -54,15 +52,21 @@ class UserController extends Controller
             return redirect()->route('users.index');
         }
 
-        return view('users.edit',['user' => $user]);
+        return view('users.edit', compact('user'));
     }
 
-    public function update($id) {
+    public function update(Request $request, $id) {
         if (!$user = User::find($id)) {
             return redirect()->route('users.index');
         }
 
+        $data = $request->only('name', 'email');
+        if ($request->password)
+            $data['password'] = bcrypt($request->password);
+
+        $user->update($data);
+
         return redirect()->route('users.index');
-    
     }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/update', [UserController::class, 'update'])->name('users.update');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 Route::get('/', function () {
