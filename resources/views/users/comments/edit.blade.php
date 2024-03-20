f28329bcd24eefc714ced42a1f6e2d0b79148e91
kaikerochadev <kaikerochadev@gmail.com>
2024-03-20 20:17:32 -0300

    Update Comment

--- resources/views/users/comments/edit.blade.php
+++ resources/views/users/comments/edit.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.app')
 
-@section('title', "Novo Comentário Para o Usuário {$user->name}")
+@section('title', "Editar o Comentário do Usuário {$user->name}")
 
 @section('content')
 
-<h1 class="text-2xl font-semibold leading-tigh py-2">Novo Comentário Para o Usuário {{$user->name}}</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Editar o Comentário do Usuário {{$user->name}}</h1>
 
 @include('includes.validation-form')
 
-<form action="{{ route('comments.store', $user->id) }}" method="POST">
+<form action="{{ route('comments.update', $comment->id) }}" method="post">
+    @method('PUT')
     @include('users.comments._partials.form')
 </form>
+
+<br>
 
 <a href="{{route('users.index')}}" class="shadow bg-green-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Voltar</a>
 
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\{
 use App\Http\Controllers\Admin\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
+Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
