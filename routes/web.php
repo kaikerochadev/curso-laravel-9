4eecee5c56e5fdba4eeb1398fe3191c7ba7edc54
kaikerochadev <kaikerochadev@gmail.com>
2024-03-22 20:12:30 -0300

    Laravel Auth

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     UserController
 };
 use App\Http\Controllers\Admin\CommentController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
 Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
 Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
 Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
@@ -23,3 +27,7 @@ Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::get('/', function () {
     return view('welcome');
 });
+
+});
+
+require __DIR__.'/auth.php';
